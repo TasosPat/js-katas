364e3b921128b32e73cc2fb60af04cf983e614f6
TasosPat <tasos.pat@outlook.com>
2025-08-15 14:23:08 +0100

    3 tests passing

--- numToString/__tests__/numToString.test.js
+++ numToString/__tests__/numToString.test.js
@@ -6,4 +6,13 @@ describe('numToString', () => {
         const expected = "5";
         expect(numToString(actual)).toBe(expected);
     });
+    test('check if a double digit number like 15 is returned as a string', () => {
+        const actual = 15;
+        const expected = "15";
+        expect(numToString(actual)).toBe(expected);
+    });
+    test('if given a number as a string, ensure the same result is returned', () => {
+        const actual = "5";
+        expect(numToString(actual)).toBe(actual);
+    });
   });
