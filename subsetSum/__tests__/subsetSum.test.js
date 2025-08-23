8c9b90f5339011c85c777e33894e7b9d7d6514e6
TasosPat <tasos.pat@outlook.com>
2025-08-23 13:53:51 +0100

    new tests added and passing

--- subsetSum/__tests__/subsetSum.test.js
+++ subsetSum/__tests__/subsetSum.test.js
@@ -16,13 +16,43 @@ describe('subsetSum', () => {
     test('check it returns a subset of the first two numbers when their sum equals the target', () => {
         const xs = [1,2,4];
         const target = 3;
-        const result = [1,2];
-        expect(subsetSum(xs, target)).toEqual(result);
+        const result = subsetSum(xs, target);
+        expect(result).not.toBe(null);
+        expect(result.reduce((acc,curr) => acc + curr, 0)).toBe(target);
     });
-    test('check it returns a subset of numbers that aren not next to each other when their sum equals the target', () => {
+    test('check it returns a subset of numbers that are not next to each other when their sum equals the target', () => {
         const xs = [1,2,3];
         const target = 4;
-        const result = [1,3];
-        expect(subsetSum(xs, target)).toEqual(result);
+        const result = subsetSum(xs, target);
+        expect(result).not.toBe(null);
+        expect(result.reduce((acc,curr) => acc + curr, 0)).toBe(target);
     });
-  });
+   
+    test('check it returns the correct subset of numbers when there are duplicates', () => {
+        const xs = [2,2,3];
+        const target = 4;
+        const result = subsetSum(xs, target);
+        expect(result).not.toBe(null);
+        expect(result.reduce((acc,curr) => acc + curr, 0)).toBe(target);
+    });
+    test('check it returns null when there are no subsets that result in the target number', () => {
+        const xs = [1,2,5];
+        const target = 10;
+
+        expect(subsetSum(xs, target)).toEqual(null);
+    });
+    test('check it returns an array with one number if that equals the target', () => {
+        const xs = [2,5,9];
+        const target = 9;
+        const result = subsetSum(xs, target);
+        expect(result).toEqual([9]);
+        
+    });
+    test('check it returns the same input array when the whole array is required to equal the target', () => {
+        const xs = [2,5,9];
+        const target = 16;
+        const result = subsetSum(xs, target);
+        expect(result).toEqual(xs);
+        
+    });
+});
